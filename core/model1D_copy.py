e51f1ccac8777db564780cc9325e959ad314eb76
anjalid247 <77317802+anjalid247@users.noreply.github.com>
2025-04-24 16:33:15 +0530

    Add files via upload

---
+++ core/model1D_copy.py
@@ -0,0 +1,111 @@
+import torch.nn as nn
+from torch.nn.utils import weight_norm
+import torch
+import numpy as np
+
+
+#################  1-D TCN Model by Mustafa et al. ###################################################
+class Chomp1d(nn.Module):
+    def __init__(self, chomp_size):
+        super(Chomp1d, self).__init__()
+        self.chomp_size = chomp_size
+
+    def forward(self, x):
+        return x[:, :, self.chomp_size:].contiguous()
+
+
+class TemporalBlock(nn.Module):
+    def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.1):
+        super(TemporalBlock, self).__init__()
+        self.conv1 = weight_norm(nn.Conv1d(n_inputs, n_outputs, kernel_size,
+                                           stride=stride, padding=padding, dilation=dilation, bias=True))
+        self.chomp1 = Chomp1d(padding)
+        self.relu1 = nn.ELU()
+        self.dropout1 = nn.Dropout(dropout)
+
+        self.conv2 = weight_norm(nn.Conv1d(n_outputs, n_outputs, kernel_size,
+                                           stride=stride, padding=padding, dilation=dilation, bias=True))
+        self.chomp2 = Chomp1d(padding)
+        self.relu2 = nn.ELU()
+        self.dropout2 = nn.Dropout(dropout)
+
+#         self.net = nn.Sequential(self.conv1, self.chomp1, self.relu1, self.dropout1,
+#                                  self.conv2, self.chomp2, self.relu2, self.dropout2)
+        self.net = nn.Sequential(self.conv1, self.relu1, self.dropout1,
+                                 self.conv2, self.relu2, self.dropout2)
+        self.downsample = nn.Conv1d(n_inputs, n_outputs, 1, bias=True) if n_inputs != n_outputs else None
+        self.relu = nn.ELU()
+        self.init_weights()
+
+    def init_weights(self):
+        nn.init.kaiming_uniform_(self.conv1.weight.data)
+        nn.init.kaiming_uniform_(self.conv2.weight.data)
+        #self.conv1.weight.data.normal_(0, 0.01)
+        #self.conv2.weight.data.normal_(0, 0.01)
+        #torch.nn.init.xavier_uniform(self.conv1.weight)
+        if self.downsample is not None:
+            nn.init.kaiming_uniform_(self.downsample.weight.data)
+            #self.downsample.weight.data.normal_(0, 0.01)
+
+    def forward(self, x):
+        out = self.net(x)
+        res = x if self.downsample is None else self.downsample(x)
+        return self.relu(out + res)
+
+
+class TemporalConvNet(nn.Module):
+    def __init__(self, num_inputs, num_channels, kernel_size=7, dropout=0.1):
+        super(TemporalConvNet, self).__init__()
+        layers = []
+        num_levels = len(num_channels)
+        for i in range(num_levels):
+            dilation_size = 2 ** i
+            in_channels = num_inputs if i == 0 else num_channels[i - 1]
+            out_channels = num_channels[i]
+            layers += [TemporalBlock(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size,
+                                     padding=int((kernel_size - 1)/2 * dilation_size), dropout=dropout)]
+
+        self.network = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.network(x)
+
+ 
+class MustafaNet(nn.Module):
+    def __init__(self):
+        super(MustafaNet, self).__init__()
+        self.tcn_local = TemporalConvNet(num_inputs=3, num_channels=[3, 6, 6, 6, 6, 6, 5], kernel_size=9, dropout=0.2)
+        self.regression = nn.Sequential(nn.Conv1d(in_channels=5, out_channels=1, kernel_size=1))
+        
+
+    def forward(self, input):
+        #out = self.tcn_local(input[:,:,:,3])  # only uncomment when you are using 2-D dataloaders
+        out = self.tcn_local(input)
+        out = self.regression(out)
+        
+        return out
+    
+class Forwardmodel(nn.Module):
+    def __init__(self, resolution_ratio=1):
+        super(Forwardmodel, self).__init__()
+        self.resolution_ratio = resolution_ratio
+        self.activation =  nn.ReLU()
+        self.cnn = nn.Sequential(nn.Conv1d(in_channels=1, out_channels=4, kernel_size=9, padding=4),
+                                 self.activation,
+                                 nn.Conv1d(in_channels=4, out_channels=4,kernel_size=7, padding=3),
+                                 self.activation,
+                                 nn.Conv1d(in_channels=4, out_channels=1,kernel_size=3, padding=1))
+
+
+        self.wavelet = nn.Conv1d(in_channels=1,
+                             out_channels=1,
+                             stride=self.resolution_ratio,
+                             kernel_size=50,
+                             padding=int((50-self.resolution_ratio+2)/2))
+
+    def forward(self, x):
+        x = self.cnn(x)
+        x = self.wavelet(x)
+        return x
+
+
---
+++ core/utils_copy5.py
@@ -0,0 +1,124 @@
+# contains utility functions
+import zipfile
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from core.utils_DANN import GradientReversal
+
+def extract(source_path, destination_path):
+    """function extracts all files from a zip file at a given source path
+    to the provided destination path"""
+    
+    with zipfile.ZipFile(source_path, 'r') as zip_ref:
+        zip_ref.extractall(destination_path)
+    
+    
+def standardize(seismic, seismic_train, model,model_train,no_wells):
+    """function standardizes data using statistics extracted from training 
+    wells
+    
+    Parameters
+    ----------
+    seismic : array_like, shape(num_traces, depth samples)
+        2-D array containing seismic section
+        
+    model : array_like, shape(num_wells, depth samples)
+        2-D array containing model section
+        
+    no_wells : int,
+        no of wells (and corresponding seismic traces) to extract.
+    """
+    torch.manual_seed(44)
+    train_indices = (np.linspace(0, len(model_train)-1, no_wells, dtype=int))  
+    seismic_org= seismic[:,0,:]
+    seismic_env= seismic[:,1,:]
+    seismic_ip=seismic[:,2,:]
+    seismic_org_train = seismic_train[:,0,:]
+    seismic_env_train = seismic_train[:,1,:]
+    seismic_ip_train = seismic_train[:,2,:]
+    seismic_normalized_org  = (seismic_org - seismic_org_train[train_indices].mean())/ seismic_org_train[train_indices].std()
+    seismic_normalized_env  = (seismic_env - seismic_env_train[train_indices].mean())/ seismic_env_train[train_indices].std()
+    seismic_normalized_ip  = (seismic_ip - seismic_ip_train[train_indices].mean())/ seismic_ip_train[train_indices].std()
+    seismic_normalized = np.dstack((seismic_normalized_org,seismic_normalized_env, seismic_normalized_ip))
+    seismic_normalized = seismic_normalized.transpose((0,2,1))
+   
+    #model_normalized = (model_split - model[train_indices].mean()) / model[train_indices].std()
+    model_normalized = (model - model_train[train_indices].mean()) / model_train[train_indices].std()
+    return seismic_normalized, model_normalized
+
+#function for unnormalization_my
+def unnormalized(AI_pred,model_train,no_wells):
+    train_indices = (np.linspace(0, len(model_train)-1, no_wells, dtype=int))
+    y_pred_un = (AI_pred * model_train[train_indices].std()) + model_train[train_indices].mean()
+    #y_pred_un = (AI_pred* model.std()) + model.mean()
+    return y_pred_un
+
+def normalization(seismic, model):
+    seismic_org= seismic[:,0,:]
+    seismic_env= seismic[:,1,:]
+    seismic_normalized_org  = (seismic_org - seismic_org.min())/ (seismic_org.max() - seismic_org.min())
+    seismic_normalized_env  = (seismic_env - seismic_env.min())/ (seismic_env.max() - seismic_env.min())
+    seismic_normalized = np.dstack((seismic_normalized_org,seismic_normalized_env))
+    seismic_normalized = seismic_normalized.transpose((0,2,1))
+    #train_indices = (np.linspace(0, len(model)-1, no_wells, dtype=int))
+    model_normalized = (model - model.min()) / (model.max() - model.min())
+    return seismic_normalized, model_normalized
+
+def unnormalized_n(AI_pred,model):
+    #train_indices = (np.linspace(0, len(model)-1, no_wells, dtype=int))
+    #model_normalized = (model - model[train_indices].mean()) / model[train_indices].std()
+    y_pred_un = (AI_pred* (model.max()-model.min())) + model.min()
+
+    return y_pred_un
+
+class GetLSTMOutput(nn.Module):
+    def forward(self, x):
+        out, _ = x
+        return out
+    
+class GRUinput(nn.Module):
+    def forward(self,x):
+        temp_x = x.transpose(-1,-2)
+        return temp_x
+    
+class GRUinv(nn.Module):
+    def forward(self,x):
+        x_inv=x.transpose(-1,-2)
+        return x_inv
+    
+class GRUunits(nn.Module):
+    def forward(self,x):
+        self.rnn1 = nn.GRU()
+
+class CustomModule(nn.Module):
+    def __init__(self):
+        super(CustomModule,self).__init__()
+        self.gru1=nn.GRU(5, 200, 3,batch_first=True, bidirectional=True, dropout=0.1)
+        #self.gru1=nn.GRU(5, 30, 3,batch_first=True, bidirectional=True, dropout=0.1)
+        #self.elu() = nn.ELU()
+        #self.gru2=nn.GRU(60, 30, 3,batch_first=True, bidirectional=True, dropout=0.2)
+        #self.gru3=nn.GRU(20,20,2, batch_first=True, bidirectional=False, dropout=0.2)
+        #self.gru4=nn.GRU(20,20, 2,batch_first=True, bidirectional=False, dropout=0.2 )
+        self.elu = nn.ELU()
+        #self.dropout = nn.Dropout(0.2)
+        self.conv1d=nn.Conv1d(in_channels=400, out_channels=1, kernel_size=1)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv1d):
+                nn.init.kaiming_uniform_(m.weight.data)
+                m.bias.data.zero_()
+
+    def forward(self,x):
+        x = x.view(x.shape[0], 5,-1)
+        x=x.permute(0,2,1)
+        x,_=self.gru1(x)
+        #x,_=self.gru2(x,h)
+        #x,h=self.gru3(x,h)
+        #x,_=self.gru4(x,h)
+        x = x.permute(0,2,1)
+        x = self.elu(x)
+        #x = self.dropout(x)
+        x = self.conv1d(x)
+        return x
+   
